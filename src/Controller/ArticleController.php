c85865d807243c7bdbf811a9f08286c5ad1b17fe
marina-a90 <marinaandjelkovic456@gmail.com>
2019-06-09 22:03:08 +0200

    kreiranje novog clanka

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,9 +5,14 @@
     use App\Entity\Article;
 
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
     class ArticleController extends Controller {
         /**
@@ -46,9 +51,49 @@
         //     return new Response('saved an article with id: ' . $article->getId());
         // }
 
+
+        /**
+         * @Route("/article/new", name="new_article")
+         * Method({"GET", "POST"})
+         */
+        public function create(Request $request) {
+            $article = new Article();
+
+            $form = $this->createFormBuilder($article)
+                ->add('title', TextType::class, array(
+                    'attr' => array('class' => 'form-control')))
+                ->add('body', TextareaType::class, array(
+                    'required' => false, 
+                    // everything required by default
+                    'attr' => array('class' => 'form-control')))
+                ->add('save', SubmitType::class, array(
+                    'label' => 'Create new article', 
+                    'attr' => array('class' => 'btn btn-primary mt-3 mb-3')))
+                ->getForm();
+
+            //iznad samo loadovanje forme, dalje handle-ovanje
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+                $article = $form->getData();
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($article);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('home');
+            }
+
+            return $this->render('articles/new.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+
         /**
          * @Route("/article/{id}", name="article_show")
          */
+        // show mora biti ispod jer je bitan redosled citanja
+        // da je iznad, citao bi /article/{id} umesto new i vodio bi na show
         public function show($id) {
             $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
